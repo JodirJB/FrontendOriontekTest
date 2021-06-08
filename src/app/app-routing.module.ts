f42fe3818fb712737060c1d8b198c9fafe0dd13c
JodirJB <jodirjb@gmail.com>
2021-06-08 15:15:10 -0400

    Creando las diferentes rutas de las app.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GetClientComponent } from './components/get-client/get-client.component';
+import { CreateClientComponent } from './components/create-client/create-client.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: GetClientComponent },
+  { path: 'create-client', component: CreateClientComponent },
+  { path: 'update-client/:id', component: CreateClientComponent },
+  { path: '**', redirectTo:'/create-client', pathMatch: 'full' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
