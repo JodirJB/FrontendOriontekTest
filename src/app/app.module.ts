d562ca435a72769029ff447698e8de6168b42168
JodirJB <jodirjb@gmail.com>
2021-06-08 15:16:28 -0400

    Declarando e importando los diferentes modulos y componentes ya creados.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
+// Modulos
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
+// Rutas
 import { AppRoutingModule } from './app-routing.module';
+
+// Componentes
 import { AppComponent } from './app.component';
+import { CreateClientComponent } from './components/create-client/create-client.component';
+import { GetClientComponent } from './components/get-client/get-client.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateClientComponent,
+    GetClientComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
